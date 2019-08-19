fcc240ae0a4adbeb52d85ed43e28cf0f12c6fe58
Brian Campbell <briancmpbll@gmail.com>
2019-08-19 16:18:52 -0700

    Initial commit

---
+++ src/build/precache-manifest.85b3c1c69dfd066b87a812020a5247a5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66168a6a54e9de4eb390168a934a9c16",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9fb020644a3562d0da9e",
+    "url": "/static/css/main.c58b48e1.chunk.css"
+  },
+  {
+    "revision": "5436b85419de92c243bf",
+    "url": "/static/js/2.53cd3df9.chunk.js"
+  },
+  {
+    "revision": "9fb020644a3562d0da9e",
+    "url": "/static/js/main.418e4fef.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
